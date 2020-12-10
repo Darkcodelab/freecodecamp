978814153705386585493f6fa56947e1bd00d552
deepan <deepanchakravarthi434@gmail.com>
2020-12-10 13:12:32 +0530

    File Upload

---
+++ data-structures/MAP_data_structure.js
@@ -0,0 +1,14 @@
+//Question - https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/create-a-map-data-structure
+
+var Map = function () {
+  this.collection = {};
+  this.add = (key, value) => {
+    this.collection[key] = value;
+  };
+  this.remove = (key) => delete this.collection[key];
+  this.get = (key) => this.collection[key];
+  this.has = (key) => this.collection[key] != undefined;
+  this.values = () => [...Object.values(this.collection)];
+  this.size = () => Object.keys(this.collection).length;
+  this.clear = () => (this.collection = {});
+};

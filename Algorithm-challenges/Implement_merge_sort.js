734a1e897b9f417e65115bf0517673521a93a4b3
deepan <deepanchakravarthi434@gmail.com>
2020-12-17 20:24:41 +0530

    File Upload

---
+++ Algorithm-challenges/Implement_merge_sort.js
@@ -0,0 +1,26 @@
+//Question - https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/implement-merge-sort
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  } else {
+    const splitIndex = Math.floor(array.length / 2);
+    return merge(
+      mergeSort(array.slice(0, splitIndex)),
+      mergeSort(array.slice(splitIndex))
+    );
+  }
+  function merge(array1, array2) {
+    let merged = [];
+    while (array1.length && array2.length) {
+      if (array1[0] < array2[0]) {
+        merged.push(array1.shift());
+      } else if (array1[0] > array2[0]) {
+        merged.push(array2.shift());
+      } else {
+        merged.push(array1.shift(), array2.shift());
+      }
+    }
+    return [...merged, ...array1, ...array2];
+  }
+}
--- Algorithm-challenges/Implement_quick_sort.js
+++ Algorithm-challenges/Implement_quick_sort.js
@@ -1,6 +1,27 @@
 //Question - https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/implement-quick-sort
 
-function quickSort(arr) {}
+function quickSort(array) {
+  if (array.length <= 1) return array;
+  if (array.length == 2) {
+    if (array[1] > array[0]) {
+      let temp = array[1];
+      array[1] = array[0];
+      array[0] = temp;
+    }
+    return array;
+  }
+  let pivot = Math.max(...array);
+  let arr1 = [],
+    arr2 = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < pivot) {
+      arr1.push(array[i]);
+    } else {
+      arr2.push(array[i]);
+    }
+  }
+  return quickSort(arr1).concat(quickSort(arr2));
+}
 
 console.log(
   quickSort([
